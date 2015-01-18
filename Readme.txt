f5b917190135783cd9902e368143734b0b8e8d7f
thelegendarymudkip <shinydanielmudkip@gmail.com>
2015-01-18 15:29:41 +0000

    v1.1
    Added code and build from local.

---
+++ Readme.txt
@@ -0,0 +1,19 @@
+---SNES Input Viewer v1.1---
+-Created by TheLegendaryMudkip-
+
+SNES Input Viewer is a program that allows you to visualize the input from a SNES controller on your monitor.
+There are a few things you need to keep in mind, however.
+
+-Limitations-
+Currently this program only has support for SNES controllers, but that may change in the future.
+
+I do not know if it supports every kind of SNES controller that can be plugged in. If it doesn't work, let me know. Details
+of the kind of SNES controller you are using would be helpful. An PM on twitch should suffice, but fell free to try and
+contact me in other ways if you cannot get hold of me there.
+
+You need to have your controller plugged in to port 0, at the moment. This is likely to change in the future.
+
+-Settings Format-
+The first line is the Red value of the background, which ranges from 0 to 255. The second line is the Green value of the background,
+and the last line is the Blue value of the background. Don't bother trying to overflow the colour data, a bitwise and
+operation with 255 (equivalent to a mod 256 operation) is performed on the values from the file.
---
+++ main.cpp
@@ -0,0 +1,191 @@
+#include <SFML\Graphics.hpp>
+#include <fstream>
+#include <Windows.h>
+sf::RenderWindow window;
+sf::Sprite cSprite, bSel, bSt, bL, bR;
+sf::Texture cTexture, ssPos, lrPos;
+sf::RectangleShape dUp(sf::Vector2f(20, 21)), dDown(sf::Vector2f(21, 21)), dLeft(sf::Vector2f(21, 21)), dRight(sf::Vector2f(21, 21))/*, bL(sf::Vector2f(5, 55)), bR(sf::Vector2f(5, 55)), bSel(sf::Vector2f(12, 30)), bSt(sf::Vector2f(12, 30))*/;
+sf::CircleShape bA(15), bB(15), bX(15), bY(15);
+std::ifstream bg;
+float dX, dY;
+bool l, r, left, right, up, down, a, b, x, y, sel, st;
+short unsigned int intR, intG, intB, i;
+std::string line;
+void initWindow()
+{
+	bg.open("settings.txt");
+	if (bg.is_open())
+	{
+		bg >> intR;
+		intR = intR & 255; //mod 256
+		bg >> intG;
+		intG = intG & 255; //mod 256
+		bg >> intB;
+		intB = intB & 255; //mod 256
+		bg.close();
+	}
+	else
+	{
+		intR = 255;
+		intG = 255;
+		intB = 255;
+		MessageBox(NULL, (LPCWSTR)L"Settings.txt not found, using defaults.", (LPCWSTR)L"Resource Missing", MB_ICONEXCLAMATION);
+	}
+	sf::ContextSettings settings;
+	window.create(sf::VideoMode(400,177),"SNES Input Display", sf::Style::Close);
+	if (!cTexture.loadFromFile("resources/controller.png"))
+	{
+		MessageBox(NULL, (LPCWSTR)L"controller.png not found.", (LPCWSTR)L"Resource Missing", MB_ICONEXCLAMATION);
+	}
+	if (!ssPos.loadFromFile("resources/ss.png"))
+	{
+		MessageBox(NULL, (LPCWSTR)L"ss.png not found.", (LPCWSTR)L"Resource Missing", MB_ICONEXCLAMATION);
+	}
+	if (!lrPos.loadFromFile("resources/LR.png"))
+	{
+		MessageBox(NULL, (LPCWSTR)L"lr.png not found.", (LPCWSTR)L"Resource Missing", MB_ICONEXCLAMATION);
+	}
+
+	dLeft.setPosition(51, 80);
+	dRight.setPosition(95, 80);
+	dUp.setPosition(74, 58);
+	dDown.setPosition(73, 102);
+	bSel.setPosition(144, 84);
+	bSt.setPosition(188, 84);
+	bY.setPosition(262, 75);
+	bA.setPosition(338, 75);
+	bX.setPosition(300, 44);
+	bB.setPosition(300, 104);
+	bL.setPosition(78, 2);
+	bR.setPosition(295, 3);
+
+	dUp.setFillColor(sf::Color(255, 0, 0, 0));
+	dDown.setFillColor(sf::Color(255, 0, 0, 0));
+	dLeft.setFillColor(sf::Color(255, 0, 0, 0));
+	dRight.setFillColor(sf::Color(255, 0, 0, 0));
+	bA.setFillColor(sf::Color(127, 0, 0, 0));
+	bB.setFillColor(sf::Color(127, 127, 0, 0));
+	bX.setFillColor(sf::Color(0, 0, 127, 0));
+	bY.setFillColor(sf::Color(0, 127, 0, 0));
+
+	cSprite.setTexture(cTexture);
+	bSel.setTexture(ssPos);
+	bSt.setTexture(ssPos);
+	bL.setTexture(lrPos);
+	bR.setTexture(lrPos);
+	bR.setOrigin(25, 2);
+	bR.setScale(-1, 1);
+}
+
+void getInput()
+{
+	x = sf::Joystick::isButtonPressed(0, 0);
+	a = sf::Joystick::isButtonPressed(0, 1);
+	b = sf::Joystick::isButtonPressed(0, 2);
+	y = sf::Joystick::isButtonPressed(0, 3);
+	l = sf::Joystick::isButtonPressed(0, 6);
+	r = sf::Joystick::isButtonPressed(0, 7);
+	sel = sf::Joystick::isButtonPressed(0, 8);
+	st = sf::Joystick::isButtonPressed(0, 9);
+	dX = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+	dY = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+}
+
+void displayInput()
+{
+	if (x)
+		bX.setFillColor(sf::Color(0, 0, 127, 255));
+	else
+		bX.setFillColor(sf::Color(0, 0, 127, 0));
+
+	if (y)
+		bY.setFillColor(sf::Color(0, 127, 0, 255));
+	else
+		bY.setFillColor(sf::Color(0, 127, 0, 0));
+
+	if (a)
+		bA.setFillColor(sf::Color(127, 0, 0, 255));
+	else
+		bA.setFillColor(sf::Color(127, 0, 0, 0));
+
+	if (b)
+		bB.setFillColor(sf::Color(127, 127, 0, 255));
+	else
+		bB.setFillColor(sf::Color(127, 127, 0, 0));
+
+	if (l)
+		bL.setColor(sf::Color(255, 255, 255, 255));
+	else
+		bL.setColor(sf::Color(255, 255, 255, 0));
+
+	if (r)
+		bR.setColor(sf::Color(255, 255, 255, 255));
+	else
+		bR.setColor(sf::Color(255, 255, 255, 0));
+
+	if (sel)
+		bSel.setColor(sf::Color(255, 255, 255, 255));
+	else
+		bSel.setColor(sf::Color(255, 255, 255, 0));
+
+	if (st)
+		bSt.setColor(sf::Color(255, 255, 255, 255));
+	else
+		bSt.setColor(sf::Color(255, 255, 255, 0));
+
+	if (dX <=-1)
+		dLeft.setFillColor(sf::Color(255, 0, 0, 255));
+	else
+		dLeft.setFillColor(sf::Color(255, 0, 0, 0));
+
+	if (dX >= 1)
+		dRight.setFillColor(sf::Color(255, 0, 0, 255));
+	else
+		dRight.setFillColor(sf::Color(255, 0, 0, 0));
+
+	if (dY <= -1)
+		dUp.setFillColor(sf::Color(255, 0, 0, 255));
+	else
+		dUp.setFillColor(sf::Color(255, 0, 0, 0));
+
+	if (dY >= 1)
+		dDown.setFillColor(sf::Color(255, 0, 0, 255));
+	else
+		dDown.setFillColor(sf::Color(255, 0, 0, 0));
+
+	window.clear(sf::Color(intR, intG, intB));
+	window.draw(cSprite);
+
+	window.draw(dUp);
+	window.draw(dDown);
+	window.draw(dLeft);
+	window.draw(dRight);
+	window.draw(bSel);
+	window.draw(bSt);
+	window.draw(bA);
+	window.draw(bB);
+	window.draw(bX);
+	window.draw(bY);
+	window.draw(bL);
+	window.draw(bR);
+
+	window.display();
+}
+
+int main()
+{
+	initWindow();
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		getInput();
+		displayInput();
+	}
+	return 0;
+}
